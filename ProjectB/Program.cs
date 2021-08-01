eb081646e58d3637aa1cb22a1a8bc9755fd1e23c
Alireza Hosseinitabar <alireza@hosseinitabar.com>
2021-08-01 18:41:33 +0430

    sample of startup reusage that fails

---
+++ ProjectB/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    // webBuilder.UseStartup<Startup>(); // this works, because the Startup.cs exists in the same DLL
+                    webBuilder.UseStartup<Base.Startup>(); // this fails, because we're using Startup.cs from another DLL.
+                });
+    }
+}
